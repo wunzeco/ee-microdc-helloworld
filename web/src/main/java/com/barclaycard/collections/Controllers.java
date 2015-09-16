7153b990603e4a0e71a1c537bf385d9ee77ec2e9
Baber Khalil <Baber.s.Khalil@googlemail.com>
2015-09-16 12:20:32 +0100

    [SS|BK] - removed unnecessary code and cleaned up gradle files.

--- web/src/main/java/com/barclaycard/collections/Controllers.java
+++ web/src/main/java/com/barclaycard/collections/Controllers.java
@@ -1,17 +1,11 @@
 package com.barclaycard.collections;
 
-import com.barclaycard.collections.controllers.ExceptionController;
 import com.barclaycard.collections.controllers.HelloController;
-import com.equalexperts.logging.OpsLogger;
-import com.equalexperts.logging.OpsLoggerFactory;
 import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.io.IOException;
-import java.nio.file.Paths;
 
 @Configuration
 public class Controllers extends WebMvcConfigurerAdapter {
@@ -26,14 +20,5 @@ public class Controllers extends WebMvcConfigurerAdapter {
         return new LayoutDialect();
     }
 
-    @Bean
-    public OpsLogger<LogMessages> logger(@Value("${opslog.location:.}") String logDir) throws IOException {
-        return new OpsLoggerFactory().setPath(Paths.get(logDir, "opslog.log")).build();
-    }
-
-    @Bean
-    public ExceptionController exceptionController(OpsLogger<LogMessages> logger){
-        return new ExceptionController(logger);
-    }
 
 }
