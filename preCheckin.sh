f9878621a681c879ed51f10444dabb0654b950cc
Baber Khalil <Baber.s.Khalil@googlemail.com>
2015-09-17 12:01:11 +0100

    [SS|BK] - removed hardcoded jar location.

--- preCheckin.sh
+++ preCheckin.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./gradlew -DfatJarLocation=./web/build/libs/collections-hello-world-0.0.2-SNAPSHOT.jar clean build stopApp startApp integrationTest
+./gradlew clean build stopApp startApp integrationTest
