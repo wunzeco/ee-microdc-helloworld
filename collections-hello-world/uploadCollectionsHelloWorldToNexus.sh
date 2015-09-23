6fb7a53846cab00c711511202e303c2aa871f3af
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2015-09-23 07:48:56 +0100

    [OI] - Add script to upload artifact to nexus

--- collections-hello-world/uploadCollectionsHelloWorldToNexus.sh
+++ collections-hello-world/uploadCollectionsHelloWorldToNexus.sh
@@ -2,7 +2,7 @@
 
 NEXUS_LOGIN=$1
 
-./uploadToNexus.sh collections-hello-world $BUILD_NUMBER jar $GIT_COMMIT "Collections-Hello-World" collections-hello-world/build/libs/collections-hello-world-0.0.2-SNAPSHOT.jar $NEXUS_LOGIN
+./uploadToNexus.sh collections-hello-world $BUILD_NUMBER jar $GIT_COMMIT "Collections-Hello-World" build/libs/collections-hello-world-0.0.2-SNAPSHOT.jar $NEXUS_LOGIN
 
 if [ $? -ne 0 ]
   then echo "[uploadToNexus.sh] failure! - failed to upload collections-hello-world artifact"
