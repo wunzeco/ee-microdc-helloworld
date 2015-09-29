b19830558dcd9273eb354b184c87fe17dec86ff3
Baber Khalil <Baber.s.Khalil@googlemail.com>
2015-09-29 16:03:38 +0100

    [BK|SS] - updated JSON data for soak test POST call to reflect new JSON schema.

--- perf/src/test/scala/com/barclaycard/collections/simulation/NotificationSimulation.scala
+++ perf/src/test/scala/com/barclaycard/collections/simulation/NotificationSimulation.scala
@@ -7,12 +7,24 @@ import scala.concurrent.duration._
 object NotificationSimulation extends Simulation {
   private val sendNotification = exec(http("send_notifications")
     .post("/sendNotifications")
-    .body(StringBody("""{
-            |    "customerId": "Bobby Smith",
-            |    "riskRating": "High",
-            |    "email" :"bs@gmail.com",
-            |    "contactPreference": "email"
-            |}""".stripMargin)).asJSON
+    .body(StringBody("""
+    {
+       "profile": {
+           "customerId": "John Smith",
+           "accountStatus": "C",
+           "cycle": "1",
+           "email": "john.smith@thesmiths.com",
+           "mobile": "07123123123",
+           "contactPreference": "sms"
+       },
+       "details": {
+           "method": "sms",
+           "from": "barclaycard",
+           "time": "2015-09-12 13:27:45",
+           "text": "We noticed that you missed a payment recently. Is there anything we can help with?"
+       }
+    }
+    """.stripMargin)).asJSON
     .check(status.is(204)))
 
   val sendNotificationsScenario = scenario("send_notifications").during(sys.env("durationInMinutes").toInt minutes) {
